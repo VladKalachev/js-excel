eae6c0251c09dc2530bdb40dfcf9db65cf892fc9
Corvax <vlvete1990@yandex.ru>
2020-05-21 13:19:28 +0300

    adding events mouse

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,29 @@ import createTable from './table.template';
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+        super($root, {
+            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        })
+    }
+
     toHTML(){
-        return createTable();
+        return createTable(20);
+    }
+
+    onClick() {
+        console.log('click')
+    }
+
+    onMousedown(event){
+        console.log('mousedown', event.target)
+    }
+
+    onMousemove() {
+        console.log('mousemove')
+    }
+
+    onMouseup() {
+        console.log('mouseup')
     }
 }
