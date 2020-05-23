cd5f67886833f03dfbb5d402a9df528951d95ac0
Corvax <vlvete1990@yandex.ru>
2020-05-23 16:34:39 +0300

    adding createStore

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,48 +1,47 @@
+import {ExcelComponent} from '@core/ExcelComponent'
 import {$} from '@core/dom'
-import {ExcelComponent} from '../../core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
-    static className = 'excel__formula'
+  static className = 'excel__formula'
 
-    constructor($root, options) {
-        super($root, {
-          name: 'Formula',
-          listeners: ['input', 'keydown'],
-          ...options
-        })
-      }
+  constructor($root, options) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'keydown'],
+      ...options
+    })
+  }
 
-    toHTML() {
-        return `
-          <div class="info">fx</div>
-          <div id="formula" class="input" contenteditable spellcheck="false">
-          </div>
-        `
+  toHTML() {
+    return `
+      <div class="info">fx</div>
+      <div id="formula" class="input" contenteditable spellcheck="false"></div>
+    `
+  }
+
+  init() {
+    super.init()
+
+    this.$formula = this.$root.find('#formula')
+
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.text())
+    })
+
+    this.$on('table:input', $cell => {
+      this.$formula.text($cell.text())
+    })
+  }
+
+  onInput(event) {
+    this.$emit('formula:input', $(event.target).text())
+  }
+
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab']
+    if (keys.includes(event.key)) {
+      event.preventDefault()
+      this.$emit('formula:done')
     }
-
-    init() {
-        super.init()
-
-        this.$formula = this.$root.find('#formula')
-
-        this.$on('table:select', $cell => {
-          this.$formula.text($cell.text())
-        })
-
-        this.$on('table:input', $cell => {
-          this.$formula.text($cell.text())
-        })
-    }
-
-    onInput(event) {
-        this.$emit('formula:input', $(event.target).text())
-    }
-
-    onKeydown(event) {
-        const keys = ['Enter', 'Tab']
-        if (keys.includes(event.key)) {
-          event.preventDefault()
-          this.$emit('formula:done')
-        }
-    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import './scss/index.scss';
+import {createStore} from './core/createStore';
+import {rootReducer} from './redux/rootReducer';
+
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 });
 
 excel.render();
