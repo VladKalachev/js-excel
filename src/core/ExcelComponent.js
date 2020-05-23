b813653b0f2a9ef007c53495ddc0151b986e8673
Corvax <vlvete1990@yandex.ru>
2020-05-23 17:32:23 +0300

    adding storeSub

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,9 +4,10 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.store = options.store
     this.emitter = options.emitter
     this.unsubscribers = []
-
+    this.storeSub = null
     this.prepare()
   }
 
@@ -21,6 +22,15 @@ export class ExcelComponent extends DomListener {
   // Уведомляем слушателей про событие event
   $emit(event, ...args) {
     this.emitter.emit(event, ...args)
+  }
+
+
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  $subscribe(fn) {
+    this.storeSub = this.store.subscribe(fn);
   }
 
   // Подписываемся на событие event
@@ -40,5 +50,6 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach(unsub => unsub())
+    this.storeSub.unsubscribers()
   }
 }
