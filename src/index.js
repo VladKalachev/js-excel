742c2692be3ef86765bc7dfe640a99ea7bc29489
Corvax <vlvete1990@yandex.ru>
2020-05-25 12:01:43 +0300

    adding page DashboardPage

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import {Excel} from './components/excel/Excel';
-import {Header} from '@/components/header/Header';
-import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Formula} from '@/components/formula/Formula';
-import {Table} from '@/components/table/Table';
-import './scss/index.scss';
-import {createStore} from './core/createStore';
-import {rootReducer} from './redux/rootReducer';
-import {storage, debounce} from './core/utils';
-import normalizeInitialState from './redux/initialState';
-// const store = createStore(rootReducer, storage('excel-state'));
-const store = createStore(rootReducer, normalizeInitialState);
+import {Router} from '@core/routes/Router'
+import {DashboardPage} from '@/pages/DashboardPage'
+import './scss/index.scss'
 
-const stateLictener = debounce(state =>
-storage('excel-state', state), 300);
-
-store.subscribe(stateLictener)
-
-const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table],
-  store
-});
-
-excel.render();
+new Router('#app', {
+  dashboard: DashboardPage
+})
